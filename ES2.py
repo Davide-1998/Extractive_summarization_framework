c9e1ea30c9d51b26b8a4ad257f8d4e50bc0bebcc
Davide-1998 <bertellidavide98@gmail.com>
2021-09-24 00:43:40 +0200

    Switched to classes instead of dictionaries

--- ES2.py
+++ ES2.py
@@ -47,82 +47,159 @@ class Scores():
             print(key, '\n')
 
 
-def saveToDisk_dict_dataset(dataset_dict, pathToFile=None):
-    if pathToFile is None:
-        pathToFile = os.getcwd() + os.sep + 'Processed_dataset.json'
-    if '.json' not in pathToFile:
-        pathToFile += '.json'
-    if os.path.isfile(pathToFile):
-        print('File {} will be overwritten', os.path.basename(pathToFile))
+class Sentence():
+    def __init__(self, sent):
+        self.tokenized = sent
+        self.scores = Scores()
 
-    out_stream = open(pathToFile, 'w')
-    json.dump(dataset_dict, out_stream)
-    out_stream.close()
+    def Print_Sentence(self):  # Only for debug purposes -> too much verbose
+        print([self.tokenized])
 
+    def Print_Scores(self):
+        self.scores.Print()
 
-def loadFromDisk_dict_dataset(pathToFile=None):
-    if pathToFile is None:
-        pathToFile = os.getcwd() + os.sep + 'Processed_dataset.json'
-    if '.json' not in pathToFile:
-        pathToFile += '.json'
-    if not os.path.isfile(pathToFile):
-        print('File {} not found', os.path.basename(pathToFile))
-        return None
-
-    in_stream = open(pathToFile, 'r')
-    loaded_dict = json.load(in_stream)
-    in_stream.close()
-    return loaded_dict
+    def info(self, verbose=True):
+        if verbose:
+            print('Tokens in sentence: {}'.format(len(self.tokenized)))
+        return len(self.tokenized)
 
 
-def process_dataset(dataset_name=None, dataset_ver=None, save=True):
-    if dataset_name is None:
-        print('No dataset specified')
-        return
+class Document():
+    def __init__(self, doc, doc_id, high):
+        self.sentences = {}
+        self.highlights = None
 
-    CNN_dataset = load_dataset(dataset_name, dataset_ver)
-    # train_CNN = CNN_dataset['train'].to_dict()
-    # print(train_CNN[0]['highlights'])
+        for sentence in doc:  # Must be pre-processed by spacy pipeline
+            if len(sentence) > 0:  # Removes punctuation only lines
+                self.add_sentence(sentence, doc_id)
+                self.add_highlights(high)
 
-    nlp = spacy.load('en_core_web_sm')  # Loads pipeline for english language
+    def add_sentence(self, sent, doc_id):
+        if not isinstance(sent, list):
+            print('Format type of input must be \'List\'')
+            return
+        if len(sent) == 1:
+            print('Warning: singleton {} detected!'.format(sent))
+        elif len(sent) == 0:  # Enforced in mathod above
+            print('Input sentence not eligible for adding operation, Ignored')
+            return
+        else:
+            sent_id = doc_id + '_{}'.format(len(self.sentences))
+            self.sentences[sent_id] = Sentence(sent)
 
-    # Generating tokenized structure for evaluation of some features
+    def add_highlights(self, high):
+        if not isinstance(high, str):
+            print('Input type must be \'string\'')
+            return
+        else:
+            self.highlights = high
 
-    # print('Tokenizing dataset:')
-    processed_CNN = {}
-    i = 0
-    with tqdm(total=len(CNN_dataset['train'])) as pbar:
-        for key in CNN_dataset['train']:
-            pbar.set_description('processing dataset: ')
-            id = key['id']
-            processed_CNN[id] = {'sentences': {},
-                                 'highlights': key['highlights']}
+    def info(self, verbose=True):
+        tot_tokens = 0
+        num_sents = len(self.sentences)
+        num_high = len(self.highlights.split('.'))
+        for key in self.sentences:
+            tot_tokens += self.sentences[key].info(verbose=False)
+        av_tokens = tot_tokens/num_sents
 
-            tokenized_article = nlp(key['article'])
-            for sentence in tokenized_article.sents:
-                temp = []
-                for token in sentence:
-                    if not token.is_punct:
-                        temp.append(token.text)
-                idx = id + '_{}'.format(len(processed_CNN[id]['sentences']))
-                processed_CNN[id]['sentences'][idx] = temp
+        if verbose:
+            print('Total tokens in document: {}\n'
+                  'Average token per document: {:0.2f}\n'
+                  'Total sentences: {}\n'
+                  'Sentences in highlights: {}\n'
+                  .format(tot_tokens, av_tokens, num_sents, num_high))
+        return {'tot_tokens': tot_tokens, 'av_tokens': av_tokens,
+                'num_sents': num_sents, 'num_high': num_high}
 
-                # Sentences feeded to article list are spacy sents classes
 
-            pbar.update(1)
-            if i == 2:
-                break
-            i += 1
-    pbar.close()
+class Dataset():
+    def __init__(self, name='Processed_dataset.json'):
+        self.documents = {}
+        self.name = name
+        if '.json' not in name:
+            name += '.json'
 
-    if save:
-        saveToDisk_dict_dataset(processed_CNN)
+    def add_document(self, doc, doc_id, high):
+        if doc_id not in self.documents:
+            self.documents[doc_id] = Document(doc, doc_id, high)
+        else:
+            print('Key already exist, run stopped to preserve consistency')
+            return
 
-    for el in processed_CNN:
-        for pair in processed_CNN[el]['sentences']:
-            print(pair, '\n', processed_CNN[el]['sentences'][pair], '\n')
-        print(processed_CNN[el]['highlights'])
+    def rename(self, name):
+        if name is not None:
+            if '.json' not in name:
+                name += '.json'
+            self.name = name
+
+    def saveToDisk(self, pathToFile=None):
+        if pathToFile is None:
+            pathToFile = os.getcwd() + os.sep + self.name
+        if '.json' not in pathToFile:
+            pathToFile += '.json'
+        if os.path.isfile(pathToFile):
+            print('File {} will be overwritten', os.path.basename(pathToFile))
+
+        out_stream = open(pathToFile, 'w')
+        json.dump(self.documents, out_stream)
+        out_stream.close()
+
+    def loadFromDisk(self, pathToFile=None):
+        if pathToFile is None:
+            pathToFile = os.getcwd() + os.sep + self.name
+        if '.json' not in pathToFile:
+            pathToFile += '.json'
+        if not os.path.isfile(pathToFile):
+            print('File {} not found', os.path.basename(pathToFile))
+            return None
+
+        in_stream = open(pathToFile, 'r')
+        loaded_dataset = json.load(in_stream)
+        in_stream.close()
+        self.documents = loaded_dataset
+
+    def process_dataset(self, dataset_in, save=True):
+
+        nlp = spacy.load('en_core_web_sm')  # Loads pipeline for english
+
+        # Generating tokenized structure for feature evaluation
+
+        i = 0
+        with tqdm(total=len(dataset_in)) as pbar:
+            for key in dataset_in:
+                pbar.set_description('processing dataset: ')
+                doc_id = key['id']
+                high = key['highlights']
+
+                tokenized_article = nlp(key['article'])
+                segmented_document = []
+                for sentence in tokenized_article.sents:
+                    tokenized_sent = []
+                    for token in sentence:
+                        if not token.is_punct:
+                            tokenized_sent.append(token.text)  # Try with lemma
+                    segmented_document.append(tokenized_sent)
+                self.add_document(segmented_document, doc_id, high)
+
+                pbar.update(1)
+                if i == 2:
+                    break
+                i += 1
+        pbar.close()
+        print('\n'*3)
+
+    def info(self, verbose=True):
+        if verbose:
+            print('Dataset name: {}\n'
+                  'Documents in dataset: {}\n'
+                  .format(self.name, len(self.documents)))
+            for doc_id in self.documents:
+                print('-'*80, '\nDocument ID: {}'.format(doc_id))
+                self.documents[doc_id].info()
 
 
 if __name__ == '__main__':
-    process_dataset('cnn_dailymail', '3.0.0')
+    CNN_dataset = load_dataset('cnn_dailymail', '3.0.0')
+    CNN_processed = Dataset(name='CNN_processed.json')
+    CNN_processed.process_dataset(CNN_dataset['train'])
+    CNN_processed.info()
